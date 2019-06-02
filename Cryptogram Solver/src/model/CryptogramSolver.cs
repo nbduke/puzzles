6b74ef02b6f8ed33e5ba0cf8724142bd43a5f09a
Nathan <nbduke@outlook.com>
2019-06-02 12:52:35 -0700

    Rewrote the algorithm to have no saved state as it made more sense.

--- Cryptogram Solver/src/model/CryptogramSolver.cs
+++ Cryptogram Solver/src/model/CryptogramSolver.cs
@@ -7,51 +7,25 @@ using Tools;
 
 namespace CryptogramSolver.Model
 {
-	public class CryptogramSolver
+	public static class CryptogramSolver
 	{
-		private readonly Dictionary<int, HashSet<string>> WordPartitions;
-
-		public static string Solve(string cryptogram, IEnumerable<string> wordList)
-		{
-			var wordPartitions = new Dictionary<int, HashSet<string>>();
-			foreach (string word in wordList)
-			{
-				if (!wordPartitions.TryGetValue(word.Length, out HashSet<string> partition))
-					partition = new HashSet<string>();
-
-				partition.Add(word);
-			}
-
-			var solver = new CryptogramSolver(wordPartitions);
-			return solver.Solve(cryptogram);
-		}
-
-		public CryptogramSolver(Dictionary<int, HashSet<string>> wordPartitions)
-		{
-			Validate.IsNotNull(wordPartitions, "wordPartitions");
-			WordPartitions = wordPartitions;
-		}
-
-		public string Solve(string cryptogram)
+		public static string Solve(string cryptogram, IEnumerable<string> dictionary)
 		{
 			Validate.IsNotNullOrEmpty(cryptogram);
+			Validate.IsNotNull(dictionary, "dictionary");
 
 			var cryptogramWords = GetWords(cryptogram);
-			if (!PartitionsExistForEachWord(cryptogramWords))
+			var wordsWithCandidates = GetCandidatesForWords(cryptogramWords, dictionary);
+
+			if (wordsWithCandidates == null)
 				return "";
 
-			var wordsWithCandidates = GetCandidatesForWords(cryptogramWords);
 			var orderedWords = wordsWithCandidates.Keys.OrderBy(
 				word => wordsWithCandidates[word].Count
 			).ToList();
 
 			var characterMap = new CharacterMap();
-			if (SolveHelper(
-				characterMap,
-				wordsWithCandidates,
-				orderedWords,
-				0
-			))
+			if (SolveHelper(characterMap, wordsWithCandidates, orderedWords, 0))
 				return characterMap.Decode(cryptogram);
 			else
 				return "";
@@ -65,35 +39,51 @@ namespace CryptogramSolver.Model
 			return words.Where(w => w.Length > 0);
 		}
 
-		private bool PartitionsExistForEachWord(IEnumerable<string> words)
-		{
-			foreach (string word in words)
-			{
-				if (!WordPartitions.ContainsKey(word.Length))
-					return false;
-			}
-
-			return true;
-		}
-
-		private Dictionary<string, List<string>> GetCandidatesForWords(
-			IEnumerable<string> cryptogramWords
+		private static Dictionary<string, List<string>> GetCandidatesForWords(
+			IEnumerable<string> cryptogramWords,
+			IEnumerable<string> dictionary
 		)
 		{
-			var candidates = new Dictionary<string, List<string>>();
+			var partitionedDictionary = PartitionDictionary(dictionary);
+			var wordsWithCandidates = new Dictionary<string, List<string>>();
+
 			foreach (string word in cryptogramWords)
 			{
-				if (!candidates.ContainsKey(word))
+				if (!wordsWithCandidates.ContainsKey(word))
 				{
-					var partition = WordPartitions[word.Length];
-					candidates.Add(
-						word,
-						partition.Where(c => IsCandidate(word, c)).ToList()
-					);
+					if (partitionedDictionary.TryGetValue(
+						word.Length,
+						out HashSet<string> partition
+					))
+					{
+						wordsWithCandidates.Add(
+							word,
+							partition.Where(c => IsCandidate(word, c)).ToList()
+						);
+					}
+					else
+					{
+						return null;
+					}
 				}
 			}
 
-			return candidates;
+			return wordsWithCandidates;
+		}
+
+		private static Dictionary<int, HashSet<string>> PartitionDictionary(
+			IEnumerable<string> dictionary
+		)
+		{
+			var partitionedDictionary = new Dictionary<int, HashSet<string>>();
+			foreach (string word in dictionary)
+			{
+				if (!partitionedDictionary.TryGetValue(word.Length, out HashSet<string> partition))
+					partition = new HashSet<string>();
+
+				partition.Add(word);
+			}
+			return partitionedDictionary;
 		}
 
 		private static bool IsCandidate(string word, string candidate)
@@ -102,25 +92,25 @@ namespace CryptogramSolver.Model
 			return map.TryAddMappings(word, candidate);
 		}
 
-		private bool SolveHelper(
+		private static bool SolveHelper(
 			CharacterMap characterMap,
 			Dictionary<string, List<string>> wordsWithCandidates,
-			List<string> words,
+			List<string> cryptogramWords,
 			int nextWordIndex
 		)
 		{
-			if (nextWordIndex >= words.Count)
+			if (nextWordIndex >= cryptogramWords.Count)
 				return true;
 
-			string encryptedWord = words[nextWordIndex];
-			foreach (string candidate in wordsWithCandidates[encryptedWord])
+			string cryptogramWord = cryptogramWords[nextWordIndex];
+			foreach (string candidate in wordsWithCandidates[cryptogramWord])
 			{
-				if (characterMap.TryAddMappings(encryptedWord, candidate))
+				if (characterMap.TryAddMappings(cryptogramWord, candidate))
 				{
 					if (SolveHelper(
 						characterMap,
 						wordsWithCandidates,
-						words,
+						cryptogramWords,
 						++nextWordIndex
 					))
 						return true;
