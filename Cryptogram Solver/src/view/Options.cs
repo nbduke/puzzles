0b6a8f4252423be90fb63038cf6a73dbc32755b3
Nathan <nbduke@outlook.com>
2019-06-08 14:41:39 -0700

    Added option to read cryptogram from a file, and added some input files.

--- Cryptogram Solver/src/view/Options.cs
+++ Cryptogram Solver/src/view/Options.cs
@@ -4,10 +4,15 @@ namespace CryptogramSolver.View
 {
 	class Options
 	{
-		[Value(0, Required = true, MetaName = "cryptogram",
+		[Value(0, Required = false, MetaName = "cryptogram",
 			HelpText = "The cryptogram puzzle"
 		)]
 		public string Cryptogram { get; set; }
+
+		[Option('f', "file", Required = false,
+			HelpText = "The path to a file containing the cryptogram to solve"
+		)]
+		public string CryptogramFile { get; set; }
 
 		[Option('d', "dictionary", Required = true, HelpText = "The dictionary file")]
 		public string Dictionary { get; set; }
--- Cryptogram Solver/src/view/Program.cs
+++ Cryptogram Solver/src/view/Program.cs
@@ -10,18 +10,36 @@ namespace CryptogramSolver.View
 	{
 		static void Main(string[] args)
 		{
-			Parser.Default.ParseArguments<Options>(args).WithParsed(SolvePuzzle);
+			Parser.Default.ParseArguments<Options>(args).WithParsed(HandleInput);
 		}
 
-		private static void SolvePuzzle(Options options)
+		private static void HandleInput(Options options)
 		{
-			Console.WriteLine(options.Cryptogram);
+			if (string.IsNullOrEmpty(options.Cryptogram) &&
+				string.IsNullOrEmpty(options.CryptogramFile))
+			{
+				Console.Error.WriteLine(
+					"You must provide either a cryptogram instance or " +
+					"the path to a file containing a cryptogram instance."
+				);
+				return;
+			}
+
+			string cryptogram = options.Cryptogram;
+			if (string.IsNullOrEmpty(cryptogram))
+			{
+				cryptogram = ReadCryptogram(options.CryptogramFile);
+			}
+
+			SolvePuzzle(cryptogram, ReadDictionary(options.Dictionary));
+		}
+
+		private static void SolvePuzzle(string cryptogram, IEnumerable<string> dictionary)
+		{
+			Console.WriteLine(cryptogram);
 			Console.WriteLine("Solving...");
 
-			string solution = Model.CryptogramSolver.Solve(
-				options.Cryptogram,
-				ReadDictionary(options.Dictionary)
-			);
+			string solution = Model.CryptogramSolver.Solve(cryptogram, dictionary);
 
 			if (string.IsNullOrEmpty(solution))
 				Console.WriteLine("We could not find a solution :(");
@@ -39,6 +57,14 @@ namespace CryptogramSolver.View
 					if (line.Length > 0)
 						yield return line;
 				}
+			}
+		}
+
+		private static string ReadCryptogram(string cryptogramFile)
+		{
+			using (var reader = new StreamReader(cryptogramFile))
+			{
+				return reader.ReadLine().Trim();
 			}
 		}
 	}
